f1c3976e5677cb36f6812a6fabcf004e179b4f21
RomanNechaev <romenkagg@gmail.com>
2023-04-09 14:47:30 +0500

    исправил замечания

--- src/main/java/task2/WordsCount.java
+++ src/main/java/task2/WordsCount.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.LineIterator;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Nechaev Roman
@@ -13,24 +14,17 @@ import java.util.*;
 public class WordsCount {
     public static void main(String[] args) {
         try {
-            LinkedHashMap<String, Long> numberOfWordInfile = getNumberOfWordInFile(new File("src/main/resources/Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt"));
-            PriorityQueue<Map.Entry<String, Long>> topTenWords = new PriorityQueue<>((x, y) -> Long.compareUnsigned(y.getValue(), x.getValue()));
-            PriorityQueue<Map.Entry<String, Long>> lastTenWords =
-                    new PriorityQueue<>(Comparator.comparingLong(Map.Entry::getValue));
-
-            for (Map.Entry<String, Long> entry : numberOfWordInfile.entrySet()) {
-                topTenWords.add(entry);
-                lastTenWords.add(entry);
-            }
-            printTopTenWords(topTenWords);
-            printLastTenWords(lastTenWords);
+            Map<String, Long> numberOfWordInfile = getNumberOfWordInFile(new File("src/main/resources/Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt"));
+            Deque<Map.Entry<String, Long>> sortedWords = numberOfWordInfile.entrySet().stream().sorted(Comparator.comparingLong(Map.Entry::getValue)).collect(Collectors.toCollection(ArrayDeque::new));
+            printLastTenWords(sortedWords);
+            printTopTenWords(sortedWords);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static LinkedHashMap<String, Long> getNumberOfWordInFile(File file) throws IOException {
-        LinkedHashMap<String, Long> numberOfWordInfile = new LinkedHashMap<>();
+    public static Map<String, Long> getNumberOfWordInFile(File file) throws IOException {
+        Map<String, Long> numberOfWordInfile = new HashMap<>();
         try (LineIterator iterator = FileUtils.lineIterator(file, "UTF-8")) {
             while (iterator.hasNext()) {
                 String[] words = iterator.nextLine().split("\s");
@@ -48,13 +42,16 @@ public class WordsCount {
         return word.replaceAll("[\\d\"'`:()| —.,?!;-]", "");
     }
 
-    private static void printTopTenWords(PriorityQueue<Map.Entry<String, Long>> topTenWords) {
+    private static void printTopTenWords(Deque<Map.Entry<String, Long>> sortedWords) {
         System.out.println("Top 10 usage word");
-        topTenWords.stream().limit(10).forEach(System.out::println);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sortedWords.pollLast());
+        }
+
     }
 
-    private static void printLastTenWords(PriorityQueue<Map.Entry<String, Long>> lastTenWords) {
+    private static void printLastTenWords(Deque<Map.Entry<String, Long>> sortedWords) {
         System.out.println("Last 10 usage word");
-        lastTenWords.stream().limit(10).forEach(System.out::println);
+        sortedWords.stream().limit(10).forEach(System.out::println);
     }
 }
